a5ac1e7e20954437a51b96a02ec9989e28defa0f
laffed <roarklaffed@gmail.com>
2020-03-10 14:57:16 -0500

    added Vid_1 Fetch Example

---
+++ Fetch_Example/script.js
@@ -0,0 +1,44 @@
+const clickMe = async () => {
+    const myVal = await fetchRetry();
+    console.log(myVal);
+}
+
+const fetchRetry = async (n = 0) => {
+    const requestOptions = {
+        method: "GET",
+        redirect: "follow"
+    }
+
+    const maxRetry = 15;
+    const apiUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/${getDateString(n)}.csv`
+
+    try {
+        const response = await fetch(apiUrl, requestOptions);
+        return response.ok ?
+            response.text() :
+            n + 1 < maxRetry ?
+            fetchRetry(n + 1) :
+            null
+    } catch (err) {
+        console.log(err);
+        return null
+    }
+}
+
+const getDateString = (n) => {
+    const date = new Date(Date.now() - daysToMs(n))
+    const format = new Intl.DateTimeFormat("en", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    })
+
+    const [month, , day, , year] = format.formatToParts(date);
+
+    return `${month.value}-${day.value}-${year.value}`
+}
+
+
+const daysToMs = (n) => {
+    return n * 24 * 60 * 60 * 1000
+}
