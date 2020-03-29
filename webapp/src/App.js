7918f3e81d9aad4800f53f5eb6b1d6782e3ad09d
laffed <roarklaffed@gmail.com>
2020-03-29 12:22:47 -0500

    moved func definition outside of useEffect

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -13,57 +13,61 @@ function App() {
   const [firstSelect, setFirstSelect] = useState(226);
   const [secondSelect, setSecondSelect] = useState(138);
 
+  //fetch github data
+  const runCall = async () => {
+    let apiValue = await fetchData();
+    const header = apiValue.split('\n').slice(0).map(line => (line.split(',')))[0].slice(4);
 
-  useEffect(() => {
-    const runCall = async () => {
-      let apiValue = await fetchData();
-      const header = apiValue.split('\n').slice(0).map(line => (line.split(',')))[0].slice(4);
+    const firstPrep = apiValue.split('\n').slice(0).map(line => (line.split(',')))[firstSelect]
+    const secondPrep = apiValue.split('\n').slice(0).map(line => (line.split(',')))[secondSelect]
 
-      const firstPrep = apiValue.split('\n').slice(0).map(line => (line.split(',')))[firstSelect]
-      const secondPrep = apiValue.split('\n').slice(0).map(line => (line.split(',')))[secondSelect]
+    const firstLbl = firstPrep[1].toUpperCase();
+    const secondLbl = secondPrep[1].toUpperCase();
 
-      const firstLbl = firstPrep[1].toUpperCase();
-      const secondLbl = secondPrep[1].toUpperCase();
+    const formatData = [];
+    for (let i = 4; i < header.length; i++) {
+      const newRow = {};
+      newRow.name = header[i];
+      newRow[firstLbl] = parseInt(firstPrep[i]);
+      newRow[secondLbl] = parseInt(secondPrep[i]);
+      formatData.push(newRow);
+    }
 
-      const formatData = [];
-      for (let i = 4; i < header.length; i++) {
-        const newRow = {};
-        newRow.name = header[i];
-        newRow[firstLbl] = parseInt(firstPrep[i]);
-        newRow[secondLbl] = parseInt(secondPrep[i]);
-        formatData.push(newRow);
-      }
+    const countriesList = apiValue.split('\n').slice(1).map(line => (line.split(',').slice(0, 2)));
+    const countriesArr = [];
+    for (let i = 0; i < countriesList.length; i++) {
+      countriesArr.push({ label: countriesList[i].reverse().join(' : '), value: i + 1 })
+    }
 
-      const countriesList = apiValue.split('\n').slice(1).map(line => (line.split(',').slice(0, 2)));
-      const countriesArr = [];
-      for (let i = 0; i < countriesList.length; i++) {
-        countriesArr.push({ label: countriesList[i].reverse().join(' : '), value: i + 1 })
-      }
+    setFirstLable(firstLbl);
+    setSecondLable(secondLbl);
+    setOptions(countriesArr)
+    setData(formatData);
+    setIsLoading(false);
+    return null;
+  }
 
-      setFirstLable(firstLbl);
-      setSecondLable(secondLbl);
-      setOptions(countriesArr)
-      setData(formatData);
-      setIsLoading(false);
+  //inner helper fetch function
+  const fetchData = async () => {
+    const requestOption = {
+      method: "GET",
+      redirect: "follow"
+    }
+
+    const apiUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv`
+
+    try {
+      const response = await fetch(apiUrl, requestOption);
+      return response.ok ? response.text() : null
+    } catch (err) {
+      console.log(err);
       return null;
     }
+  }
 
-    const fetchData = async () => {
-      const requestOption = {
-        method: "GET",
-        redirect: "follow"
-      }
-
-      const apiUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv`
-
-      try {
-        const response = await fetch(apiUrl, requestOption);
-        return response.ok ? response.text() : null
-      } catch (err) {
-        console.log(err);
-        return null;
-      }
-    }
+  //calls api everytime select changes. 
+  //refactor to cache data
+  useEffect(() => {
     runCall();
   }, [firstSelect, secondSelect])
 
